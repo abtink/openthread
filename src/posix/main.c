a089cdc7f763933eebacde6b7871f3db8ed97fd6
Abtin Keshavarzian <abtink@google.com>
2019-12-27 13:17:30 -0800

    [posix-app] adding support for TREL over IPv6/UDP (#4440)
    This commit implements TREL IPv6/UDP platform under POSIX App. The
    implementation uses datagram sockets to exchange packets over a given
    network interface (netif). The interface name can hard-coded in the
    build using `OPENTHREAD_CONFIG_POSIX_APP_TREL_INTERFACE_NAME` config
    or be specified as part of the input program arguments (using
    `--trel-interface` or `-t` option).

--- src/posix/main.c
+++ src/posix/main.c
@@ -108,6 +108,7 @@ enum
     OT_POSIX_OPT_DRY_RUN        = 'n',
     OT_POSIX_OPT_HELP           = 'h',
     OT_POSIX_OPT_INTERFACE_NAME = 'I',
+    OT_POSIX_OPT_TREL_INTERFACE = 't',
     OT_POSIX_OPT_TIME_SPEED     = 's',
     OT_POSIX_OPT_VERBOSE        = 'v',
 
@@ -128,6 +129,7 @@ static const struct option kOptions[] = {
     {"radio-version", no_argument, NULL, OT_POSIX_OPT_RADIO_VERSION},
     {"real-time-signal", required_argument, NULL, OT_POSIX_OPT_REAL_TIME_SIGNAL},
     {"time-speed", required_argument, NULL, OT_POSIX_OPT_TIME_SPEED},
+    {"trel-interface", required_argument, NULL, OT_POSIX_OPT_TREL_INTERFACE},
     {"verbose", no_argument, NULL, OT_POSIX_OPT_VERBOSE},
     {0, 0, 0, 0}};
 
@@ -146,6 +148,7 @@ static void PrintUsage(const char *aProgramName, FILE *aStream, int aExitCode)
             "    -n  --dry-run                 Just verify if arguments is valid and radio spinel is compatible.\n"
             "        --radio-version           Print radio firmware version.\n"
             "    -s  --time-speed factor       Time speed up factor.\n"
+            "    -t  --trel-interface name   Interface name for TREL platform (e.g., wlan0 netif).\n"
             "    -v  --verbose                 Also log to stderr.\n",
             aProgramName);
 #ifdef __linux__
@@ -177,7 +180,7 @@ static void ParseArg(int aArgCount, char *aArgVector[], PosixConfig *aConfig)
 #if OPENTHREAD_CONFIG_BACKBONE_ROUTER_ENABLE
                                  "B:"
 #endif
-                                 "d:hI:ns:v",
+                                 "d:hI:t:ns:v",
                                  kOptions, &index);
 
         if (option == -1)
@@ -201,6 +204,9 @@ static void ParseArg(int aArgCount, char *aArgVector[], PosixConfig *aConfig)
             aConfig->mPlatformConfig.mBackboneInterfaceName = optarg;
             break;
 #endif
+        case OT_POSIX_OPT_TREL_INTERFACE:
+            aConfig->mPlatformConfig.mTrelInterface = optarg;
+            break;
         case OT_POSIX_OPT_DRY_RUN:
             aConfig->mIsDryRun = true;
             break;
