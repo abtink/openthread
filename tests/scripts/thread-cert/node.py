dcae5ba42351b4dbad7f5fec2cd67ada29d855e5
Abtin Keshavarzian <abtink@google.com>
2021-06-18 12:57:22 -0700

    [srp-server] add sub-type support
    add `Service::Description`
    
    add linked list of service descriptions in host
    
    remove find service from host
    
    start of the main part
    
    change matches
    
    new version of new for service
    
    update remove servcie()
    
    add GetInstaceName and GetServiceName()
    
    add MergeServicesAnd
    
    remove addservice
    
    remove service new and constructor + make mHost and mDesc reference
    
    move host to description
    
    add get sub label
    
    add get sub label

--- tests/scripts/thread-cert/node.py
+++ tests/scripts/thread-cert/node.py
@@ -890,8 +890,8 @@ class NodeImpl:
                 service_list.append(service)
                 continue
 
-            # 'port', 'priority', 'weight'
-            for i in range(0, 3):
+            # 'subtypes', port', 'priority', 'weight'
+            for i in range(0, 4):
                 key_value = lines.pop(0).strip().split(':')
                 service[key_value[0].strip()] = key_value[1].strip()
 
