33663beac65078bb36a2350608915213f8d0a810
Abtin Keshavarzian <abtink@google.com>
2024-02-29 13:01:35 -0800

    (to squash later) enhance `simulation/mdns_socket.c`
    - Create IPv6 socket in addition to IPv4
    - Send and receive mDNS messages on both IPv6 and IPv4
    - Simplify logging and handling errors
    - Implement `otPlatMdnsSendUnicast()`

--- examples/platforms/simulation/mdns_socket.c
+++ examples/platforms/simulation/mdns_socket.c
@@ -28,6 +28,7 @@
 
 #include "platform-simulation.h"
 
+#include <openthread/nat64.h>
 #include <openthread/platform/mdns_socket.h>
 
 #if OPENTHREAD_CONFIG_MULTICAST_DNS_ENABLE
@@ -41,6 +42,8 @@
 #include <openthread/ip6.h>
 
 #include <arpa/inet.h>
+#include <errno.h>
+#include <net/if.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,73 +51,237 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include "simul_utils.h"
 #include "utils/code_utils.h"
 
 #define MAX_BUFFER_SIZE 1600
 
-static bool sEnabled = false;
-static int  sMdnsFd4 = -1;
+#define MDNS_PORT 5353
+
+static bool     sEnabled = false;
+static uint32_t sInfraIfIndex;
+static int      sMdnsFd4 = -1;
+static int      sMdnsFd6 = -1;
+
+/* this is a portability hack */
+#ifndef IPV6_ADD_MEMBERSHIP
+#ifdef IPV6_JOIN_GROUP
+#define IPV6_ADD_MEMBERSHIP IPV6_JOIN_GROUP
+#endif
+#endif
+
+#ifndef IPV6_DROP_MEMBERSHIP
+#ifdef IPV6_LEAVE_GROUP
+#define IPV6_DROP_MEMBERSHIP IPV6_LEAVE_GROUP
+#endif
+#endif
+
+#define VerifyOrDie(aCondition, aErrMsg)                                        \
+    do                                                                          \
+    {                                                                           \
+        if (!(aCondition))                                                      \
+        {                                                                       \
+            fprintf(stderr, "\n\r" aErrMsg ". errono:%s\n\r", strerror(errno)); \
+            exit(1);                                                            \
+        }                                                                       \
+    } while (false)
+
+static void SetReuseAddrPort(int aFd)
+{
+    int ret;
+    int yes = 1;
+
+    ret = setsockopt(aFd, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes));
+    VerifyOrDie(ret >= 0, "setsocketopt(SO_REUSEADDR) failed");
+
+    ret = setsockopt(aFd, SOL_SOCKET, SO_REUSEPORT, (char *)&yes, sizeof(yes));
+    VerifyOrDie(ret >= 0, "setsocketopt(SO_REUSEPORT) failed");
+}
+
+static void OpenIp4Socket(uint32_t aInfraIfIndex)
+{
+    OT_UNUSED_VARIABLE(aInfraIfIndex);
+
+    struct sockaddr_in addr;
+    int                fd;
+    int                ret;
+    uint8_t            u8;
+    int                value;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    VerifyOrDie(fd >= 0, "socket() failed");
+
+#ifdef __linux__
+    {
+        char        nameBuffer[IF_NAMESIZE];
+        const char *ifname;
+
+        ifname = if_indextoname(aInfraIfIndex, nameBuffer);
+        VerifyOrDie(ifname != NULL, "if_indextoname() failed");
+
+        ret = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, ifname, strlen(ifname));
+        VerifyOrDie(ret >= 0, "setsocketopt(SO_BINDTODEVICE) failed");
+    }
+#else
+    value = aInfraIfIndex;
+    ret   = setsockopt(fd, IPPROTO_IP, IP_BOUND_IF, &value, sizeof(value));
+#endif
+
+    u8  = 255;
+    ret = setsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL, &u8, sizeof(u8));
+    VerifyOrDie(ret >= 0, "setsocketopt(IP_MULTICAST_TTL) failed");
+
+    value = 255;
+    ret   = setsockopt(fd, IPPROTO_IP, IP_TTL, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IP_TTL) failed");
+
+    u8  = 1;
+    ret = setsockopt(fd, IPPROTO_IP, IP_MULTICAST_LOOP, &u8, sizeof(u8));
+    VerifyOrDie(ret >= 0, "setsocketopt(IP_MULTICAST_LOOP) failed");
+
+    SetReuseAddrPort(fd);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(MDNS_PORT);
+
+    ret = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+    VerifyOrDie(ret >= 0, "bind() failed");
+
+    sMdnsFd4 = fd;
+}
+
+static void JoinOrLeaveIp4MulticastGroup(bool aJoin, uint32_t aInfraIfIndex)
+{
+    OT_UNUSED_VARIABLE(aInfraIfIndex);
+
+    struct ip_mreq mreq;
+    int            ret;
+
+    memset(&mreq, 0, sizeof(mreq));
+    mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.251");
+
+    if (aJoin)
+    {
+        // Suggested workaround for netif not dropping
+        // a previous multicast membership.
+        setsockopt(sMdnsFd4, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
+    }
+
+    ret = setsockopt(sMdnsFd4, IPPROTO_IP, aJoin ? IP_ADD_MEMBERSHIP : IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
+    VerifyOrDie(ret >= 0, "setsocketopt(IP_ADD/DROP_MEMBERSHIP) failed");
+}
+
+static void OpenIp6Socket(uint32_t aInfraIfIndex)
+{
+    OT_UNUSED_VARIABLE(aInfraIfIndex);
+
+    struct sockaddr_in6 addr6;
+    int                 fd;
+    int                 ret;
+    int                 value;
+
+    fd = socket(AF_INET6, SOCK_DGRAM, 0);
+    VerifyOrDie(fd >= 0, "socket() failed");
+
+#ifdef __linux__
+    {
+        char        nameBuffer[IF_NAMESIZE];
+        const char *ifname;
+
+        ifname = if_indextoname(aInfraIfIndex, nameBuffer);
+        VerifyOrDie(ifname != NULL, "if_indextoname() failed");
+
+        ret = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, ifname, strlen(ifname));
+        VerifyOrDie(ret >= 0, "setsocketopt(SO_BINDTODEVICE) failed");
+    }
+#else
+    value = aInfraIfIndex;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_BOUND_IF, &value, sizeof(value));
+#endif
+
+    value = 255;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_MULTICAST_HOPS, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IPV6_MULTICAST_HOPS) failed");
+
+    value = 255;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_UNICAST_HOPS, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IPV6_UNICAST_HOPS) failed");
+
+    value = 1;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IPV6_V6ONLY) failed");
+
+    value = aInfraIfIndex;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_MULTICAST_IF, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IPV6_MULTICAST_IF) failed");
+
+    value = 1;
+    ret   = setsockopt(fd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP, &value, sizeof(value));
+    VerifyOrDie(ret >= 0, "setsocketopt(IPV6_MULTICAST_LOOP) failed");
+
+    SetReuseAddrPort(fd);
+
+    memset(&addr6, 0, sizeof(addr6));
+    addr6.sin6_family = AF_INET6;
+    addr6.sin6_port   = htons(MDNS_PORT);
+
+    ret = bind(fd, (struct sockaddr *)&addr6, sizeof(addr6));
+    VerifyOrDie(ret >= 0, "bind() failed");
+
+    sMdnsFd6 = fd;
+}
+
+static void JoinOrLeaveIp6MulticastGroup(bool aJoin, uint32_t aInfraIfIndex)
+{
+    struct ipv6_mreq mreq6;
+    int              ret;
+
+    memset(&mreq6, 0, sizeof(mreq6));
+
+    inet_pton(AF_INET6, "ff02::fb", &mreq6.ipv6mr_multiaddr);
+    mreq6.ipv6mr_interface = (int)aInfraIfIndex;
+
+    if (aJoin)
+    {
+        // Suggested workaround for netif not dropping
+        // a previous multicast membership.
+        setsockopt(sMdnsFd6, IPPROTO_IPV6, IPV6_DROP_MEMBERSHIP, &mreq6, sizeof(mreq6));
+    }
+
+    ret = setsockopt(sMdnsFd6, IPPROTO_IPV6, aJoin ? IPV6_ADD_MEMBERSHIP : IPV6_DROP_MEMBERSHIP, &mreq6, sizeof(mreq6));
+    VerifyOrDie(ret >= 0, "setsocketopt(IP6_ADD/DROP_MEMBERSHIP) failed");
+}
 
 otError otPlatMdnsSetListeningEnabled(otInstance *aInstance, bool aEnable, uint32_t aInfraIfIndex)
 {
     OT_UNUSED_VARIABLE(aInstance);
-    OT_UNUSED_VARIABLE(aInfraIfIndex);
-
-    const char    *errMessage = NULL;
-    struct ip_mreq mreq;
 
     if (aEnable)
     {
-        struct sockaddr_in addr;
-        int                yes = 1;
-        int                fd;
-        int                ret;
-
         otEXPECT(!sEnabled);
 
-        fd = socket(AF_INET, SOCK_DGRAM, 0);
-        otEXPECT_ACTION(fd >= 0, errMessage = "socket() failed for sMulticastRx");
+        OpenIp4Socket(aInfraIfIndex);
+        JoinOrLeaveIp4MulticastGroup(/* aJoin */ true, aInfraIfIndex);
+        OpenIp6Socket(aInfraIfIndex);
+        JoinOrLeaveIp6MulticastGroup(/* aJoin */ true, aInfraIfIndex);
 
-        ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes));
-        otEXPECT_ACTION(ret >= 0, errMessage = "setsocketopt(SO_REUSEADDR) failed");
-
-        ret = setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, (char *)&yes, sizeof(yes));
-        otEXPECT_ACTION(ret >= 0, errMessage = "setsocketopt(SO_REUSEPORT) failed");
-
-        memset(&addr, 0, sizeof(addr));
-        addr.sin_family      = AF_INET;
-        addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        addr.sin_port        = htons(5353);
-        ret                  = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
-        otEXPECT_ACTION(ret >= 0, errMessage = "bind() failed");
-
-        mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.251");
-        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-        ret                       = setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&mreq, sizeof(mreq));
-        otEXPECT_ACTION(ret >= 0, errMessage = "setsocketopt(IP_ADD_MEMBERSHIP) failed");
-
-        sMdnsFd4 = fd;
-        sEnabled = true;
+        sEnabled      = true;
+        sInfraIfIndex = aInfraIfIndex;
     }
     else
     {
         otEXPECT(sEnabled);
 
-        mreq.imr_multiaddr.s_addr = inet_addr("224.0.0.251");
-        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-        setsockopt(sMdnsFd4, IPPROTO_IP, IP_DROP_MEMBERSHIP, (char *)&mreq, sizeof(mreq));
-
+        JoinOrLeaveIp4MulticastGroup(/* aJoin */ false, aInfraIfIndex);
+        JoinOrLeaveIp6MulticastGroup(/* aJoin */ false, aInfraIfIndex);
         close(sMdnsFd4);
+        close(sMdnsFd6);
         sEnabled = false;
     }
 
 exit:
-    if (errMessage != NULL)
-    {
-        fprintf(stderr, "\n\rotPlatMdnsSetEnabled(): %s\n\r", errMessage);
-        exit(1);
-    }
-
     return OT_ERROR_NONE;
 }
 
@@ -123,29 +290,39 @@ void otPlatMdnsSendMulticast(otInstance *aInstance, otMessage *aMessage, uint32_
     OT_UNUSED_VARIABLE(aInstance);
     OT_UNUSED_VARIABLE(aInfraIfIndex);
 
-    struct sockaddr_in addr;
-    uint8_t            buffer[MAX_BUFFER_SIZE];
-    uint16_t           length;
-    int                bytes;
+    uint8_t  buffer[MAX_BUFFER_SIZE];
+    uint16_t length;
+    int      bytes;
 
     otEXPECT(sEnabled);
 
     length = otMessageRead(aMessage, 0, buffer, sizeof(buffer));
     otMessageFree(aMessage);
 
-    // fprintf(stderr, "\n\rotPlatMdnsSendMulticast() len:%u", length);
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family      = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("224.0.0.251");
-    addr.sin_port        = htons(5353);
-
-    bytes = sendto(sMdnsFd4, buffer, length, 0, (struct sockaddr *)&addr, sizeof(addr));
-
-    if ((bytes < 0) || (bytes != length))
     {
-        fprintf(stderr, "\ntPlatMdnsSendMulticast() failed");
-        exit(1);
+        struct sockaddr_in addr;
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family      = AF_INET;
+        addr.sin_addr.s_addr = inet_addr("224.0.0.251");
+        addr.sin_port        = htons(MDNS_PORT);
+
+        bytes = sendto(sMdnsFd4, buffer, length, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+        VerifyOrDie((bytes == length), "sendTo(sMdnsFd4) failed");
+    }
+
+    {
+        struct sockaddr_in6 addr6;
+
+        memset(&addr6, 0, sizeof(addr6));
+        addr6.sin6_family = AF_INET6;
+        addr6.sin6_port   = htons(MDNS_PORT);
+        inet_pton(AF_INET6, "ff02::fb", &addr6.sin6_addr);
+
+        bytes = sendto(sMdnsFd6, buffer, length, 0, (struct sockaddr *)&addr6, sizeof(addr6));
+
+        VerifyOrDie((bytes == length), "sendTo(sMdnsFd6) failed");
     }
 
 exit:
@@ -155,20 +332,54 @@ exit:
 void otPlatMdnsSendUnicast(otInstance *aInstance, otMessage *aMessage, const otPlatMdnsAddressInfo *aAddress)
 {
     OT_UNUSED_VARIABLE(aInstance);
-    OT_UNUSED_VARIABLE(aAddress);
+
+    otIp4Address ip4Addr;
+    uint8_t      buffer[MAX_BUFFER_SIZE];
+    uint16_t     length;
+    int          bytes;
+
+    otEXPECT(sEnabled);
+
+    length = otMessageRead(aMessage, 0, buffer, sizeof(buffer));
     otMessageFree(aMessage);
+
+    if (otIp4FromIp4MappedIp6Address(&aAddress->mAddress, &ip4Addr) == OT_ERROR_NONE)
+    {
+        struct sockaddr_in addr;
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        memcpy(&addr.sin_addr.s_addr, &ip4Addr, sizeof(otIp4Address));
+        addr.sin_port = htons(MDNS_PORT);
+
+        bytes = sendto(sMdnsFd4, buffer, length, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+        VerifyOrDie((bytes == length), "sendTo(sMdnsFd4) failed");
+    }
+    else
+    {
+        struct sockaddr_in6 addr6;
+
+        memset(&addr6, 0, sizeof(addr6));
+        addr6.sin6_family = AF_INET6;
+        addr6.sin6_port   = htons(MDNS_PORT);
+        memcpy(&addr6.sin6_addr, &aAddress->mAddress, sizeof(otIp6Address));
+
+        bytes = sendto(sMdnsFd6, buffer, length, 0, (struct sockaddr *)&addr6, sizeof(addr6));
+
+        VerifyOrDie((bytes == length), "sendTo(sMdnsFd6) failed");
+    }
+
+exit:
+    return;
 }
 
 void platformMdnsSocketUpdateFdSet(fd_set *aReadFdSet, int *aMaxFd)
 {
     otEXPECT(sEnabled);
 
-    FD_SET(sMdnsFd4, aReadFdSet);
-
-    if (*aMaxFd < sMdnsFd4)
-    {
-        *aMaxFd = sMdnsFd4;
-    }
+    utilsAddFdToFdSet(sMdnsFd4, aReadFdSet, aMaxFd);
+    utilsAddFdToFdSet(sMdnsFd6, aReadFdSet, aMaxFd);
 
 exit:
     return;
@@ -190,30 +401,43 @@ void platformMdnsSocketProcess(otInstance *aInstance, const fd_set *aReadFdSet)
         memset(&sockaddr, 0, sizeof(sockaddr));
         rval = recvfrom(sMdnsFd4, (char *)&buffer, sizeof(buffer), 0, (struct sockaddr *)&sockaddr, &len);
 
-        if (rval < 0)
-        {
-            fprintf(stderr, "\n\rplatformMdnsSocketProcess() - recvfrom() failed\n\r");
-            exit(1);
-        }
+        VerifyOrDie(rval >= 0, "recvfrom() failed");
 
         message = otIp6NewMessage(aInstance, NULL);
+        VerifyOrDie(message != NULL, "otIp6NewMessage() failed");
 
-        if (message == NULL)
-        {
-            fprintf(stderr, "\n\rplatformMdnsSocketProcess() - `otIp6NewMessage()` failed\n\r");
-            exit(1);
-        }
-
-        if (otMessageAppend(message, buffer, (uint16_t)rval) != OT_ERROR_NONE)
-        {
-            fprintf(stderr, "\n\rplatformMdnsSocketProcess() - `otMessageAppend()` failed\n\r");
-            exit(1);
-        }
+        VerifyOrDie(otMessageAppend(message, buffer, (uint16_t)rval) == OT_ERROR_NONE, "otMessageAppend() failed");
 
         memset(&addrInfo, 0, sizeof(addrInfo));
-        addrInfo.mPort = 5353;
+        otIp4ToIp4MappedIp6Address((otIp4Address *)(&sockaddr.sin_addr.s_addr), &addrInfo.mAddress);
+        addrInfo.mPort         = MDNS_PORT;
+        addrInfo.mInfraIfIndex = sInfraIfIndex;
 
-        // fprintf(stderr, "\n\rCalling otPlatMdnsSendMulticast(len:%u)", (uint16_t)rval);
+        otPlatMdnsHandleReceive(aInstance, message, /* aInUnicast */ false, &addrInfo);
+    }
+
+    if (FD_ISSET(sMdnsFd6, aReadFdSet))
+    {
+        uint8_t               buffer[MAX_BUFFER_SIZE];
+        struct sockaddr_in6   sockaddr6;
+        otPlatMdnsAddressInfo addrInfo;
+        otMessage            *message;
+        socklen_t             len = sizeof(sockaddr6);
+        ssize_t               rval;
+
+        memset(&sockaddr6, 0, sizeof(sockaddr6));
+        rval = recvfrom(sMdnsFd6, (char *)&buffer, sizeof(buffer), 0, (struct sockaddr *)&sockaddr6, &len);
+        VerifyOrDie(rval >= 0, "recvfrom(sMdnsFd6) failed");
+
+        message = otIp6NewMessage(aInstance, NULL);
+        VerifyOrDie(message != NULL, "otIp6NewMessage() failed");
+
+        VerifyOrDie(otMessageAppend(message, buffer, (uint16_t)rval) == OT_ERROR_NONE, "otMessageAppend() failed");
+
+        memset(&addrInfo, 0, sizeof(addrInfo));
+        memcpy(&addrInfo.mAddress, &sockaddr6.sin6_addr, sizeof(otIp6Address));
+        addrInfo.mPort         = MDNS_PORT;
+        addrInfo.mInfraIfIndex = sInfraIfIndex;
 
         otPlatMdnsHandleReceive(aInstance, message, /* aInUnicast */ false, &addrInfo);
     }
@@ -264,6 +488,26 @@ OT_TOOL_WEAK otError otMessageAppend(otMessage *aMessage, const void *aBuf, uint
     OT_UNUSED_VARIABLE(aLength);
 
     fprintf(stderr, "\n\rWeak otMessageFree() is incorrectly used\n\r");
+    exit(1);
+
+    return OT_ERROR_NOT_IMPLEMENTED;
+}
+
+OT_TOOL_WEAK void otIp4ToIp4MappedIp6Address(const otIp4Address *aIp4Address, otIp6Address *aIp6Address)
+{
+    OT_UNUSED_VARIABLE(aIp4Address);
+    OT_UNUSED_VARIABLE(aIp6Address);
+
+    fprintf(stderr, "\n\rWeak otIp4ToIp4MappedIp6Address() is incorrectly used\n\r");
+    exit(1);
+}
+
+OT_TOOL_WEAK otError otIp4FromIp4MappedIp6Address(const otIp6Address *aIp6Address, otIp4Address *aIp4Address)
+{
+    OT_UNUSED_VARIABLE(aIp6Address);
+    OT_UNUSED_VARIABLE(aIp4Address);
+
+    fprintf(stderr, "\n\rWeak otIp4FromIp4MappedIp6Address() is incorrectly used\n\r");
     exit(1);
 
     return OT_ERROR_NOT_IMPLEMENTED;
